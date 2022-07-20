fc7465744063c9dba4421a062ddd1718457f14cc
shifeilong <109640527+ssfl@users.noreply.github.com>
2022-07-20 20:30:44 +0800

    Revert "update"
    This reverts commit 11f99f498948006240a09b60ae080438c8cdd2c7.

--- assets/js/home.js
+++ assets/js/home.js
@@ -28,24 +28,24 @@ setInterval(function () {
     };
 }, 60000);// visit
 function updateVisit() {
-    $.getJSON('https://matomo.flpro.cn/', {
+    $.getJSON('https://vps.irockbunny.com/analytics/', {
         'module': 'API',
         'method': 'VisitsSummary.getUniqueVisitors',
         'idSite': '1',
         'period': 'day',
         'date': 'today',
         'format': 'JSON',
-        'token_auth': '5d491a6c5783227bed26941ba1c2ca8c'
+        'token_auth': 'b20d2b5002a1241da41853f33eccccdd'
     }, function (data) {
         $('#today-visitors img').attr('src', `https://img.shields.io/badge/%E4%BB%8A%E6%97%A5%E8%AE%BF%E5%AE%A2-${encodeURIComponent(data.value)}-brightgreen.svg`);
     });
-    $.getJSON('https://matomo.flpro.cn/', {
+    $.getJSON('https://vps.irockbunny.com/analytics/', {
         'module': 'API',
         'method': 'Live.getCounters',
         'idSite': '1',
         'lastMinutes': '30',
         'format': 'JSON',
-        'token_auth': '5d491a6c5783227bed26941ba1c2ca8c'
+        'token_auth': 'b20d2b5002a1241da41853f33eccccdd'
     }, function (data) {
         $('#live-visitors img').attr('src', `https://img.shields.io/badge/%E5%BD%93%E5%89%8D%E5%9C%A8%E7%BA%BF-${encodeURIComponent(data[0].visitors)}-brightgreen.svg`);
     });
